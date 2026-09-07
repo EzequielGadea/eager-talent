23daddb3c0e2f23ddcae2e49b8e8af714b2686eb
el-fossa <fossatti.martin@gmail.com>
2026-09-07 17:59:06 -0300

    prueba de agregar querie a DB y llamado trpc para consumir data

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,32 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { z } from "zod";
+import { publicProcedure } from "~/server/api/trpc";
+import { listCandidates } from "~logic/candidateService";
 
-export const appRouter = createTRPCRouter({});
+
+
+export const candidateRouter = createTRPCRouter({
+    fetchCandidates: publicProcedure
+        .input(
+            z.object({ //TODO agregar parametros para filtros, todos opcionales
+                //<param1>: z.string().optional()
+                //<param2>: z.number()
+            }).optional()
+        ) 
+        .query(async ({input}) => {
+            const result = await listCandidates(/*input*/);
+            console.log("antes de ir a front");
+            return {candidates: result};
+        }),
+})
+
+export const appRouter = createTRPCRouter({
+    candidate: candidateRouter,
+});
 
 export type AppRouter = typeof appRouter;
+export type CandidateRouter = typeof candidateRouter;
 
 export const createCaller = createCallerFactory(appRouter);
+export const createCallerCandidate = createCallerFactory(candidateRouter);
 
