b0b71f7b849ebca2b351d4fbc5dbc3631593f2ce
el-fossa <fossatti.martin@gmail.com>
2026-09-07 17:59:06 -0300

    prueba de agregar querie a DB y llamado trpc para consumir data

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,11 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { z } from "zod";
+import { publicProcedure } from "~/server/api/trpc";
+import { listCandidates } from "~logic/candidateService";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+    candidate: candidateRouter,
+});
 
 export type AppRouter = typeof appRouter;
 
