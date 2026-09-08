7f4acd60f976270735ebadeaec11b1af971e8cff
brunovecchio <brunovecchio9@gmail.com>
2026-09-07 23:44:43 -0300

    Inicio de la pantalla de nuevo candidato

---
+++ src/app/(authenticated)/candidatos/_components/buttonRedirect.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "~/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function ButtonRedirect() {
+    const router = useRouter();
+    return (        <div className="flex flex-col gap-4 align-center justify-center">
+        <Button onClick={() => redirect()}>
+            Nuevo Candidato
+        </Button>
+    </div>);
+
+
+        function redirect() {
+      router.push("/candidatos/nuevoCandidato");
+    }
+}
---
+++ src/app/(authenticated)/candidatos/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import  ButtonRedirect  from "./_components/buttonRedirect";
+
+
+export default async function DashboardPage() {
+  return (
+    <div> <ButtonRedirect /> </div>
+    
+  );
+}
