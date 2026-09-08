15bd5deb7bcdbfa148249758d6931780816e02df
brunovecchio <brunovecchio9@gmail.com>
2026-09-08 19:44:44 -0300

    agregue una consulta a la base para las areas que se traen en professional-profile.tsx(Deje comentarios en page.tsx de new-candidate

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,9 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { areaRouter } from "./routers/area";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+    area: areaRouter,
+});
 
 export type AppRouter = typeof appRouter;
 
