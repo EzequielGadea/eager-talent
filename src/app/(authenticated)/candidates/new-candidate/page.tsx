f754e336f08a7674d6e5d5413acc21862d227efc
Ismael Valentin Acosta Cardoso <ismael.valentin.acosta.cardoso@fing.edu.uy>
2026-09-09 02:27:20 -0300

    comienzo de campo de etiquetas, traer de la base enumerados source y english level

--- src/app/(authenticated)/candidates/new-candidate/page.tsx
+++ src/app/(authenticated)/candidates/new-candidate/page.tsx
@@ -5,7 +5,7 @@ import Loading from "~/components/ui/loading";
 import { Button } from "~/components/ui/button";
 import  PersonalData  from "./_components/personal-data";
 import ProfessionalProfile from "./_components/professional-profile";
-import SourceAndTags from "./_components/source-and-labels";
+import SourceAndTags from "./_components/source-and-tags";
 import EducationAndFiles from "./_components/education-and-files";
 import NewCandidateButton from "./_components/new-candidate-button";
 import NewCandidateForm from "./_components/new-candidate-form";
