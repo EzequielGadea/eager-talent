702f17778833115121d390342ba02f868e7cbb62
Ismael Valentin Acosta Cardoso <ismael.valentin.acosta.cardoso@fing.edu.uy>
2026-09-09 00:49:41 -0300

    Agrega fetcheos de la base de datos para los select y uso de zod

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,16 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { applicantRouter } from "./routers/applicant";
 import { areaRouter } from "./routers/area";
+import { roleRouter } from "./routers/role";
+import { seniorityRouter } from "./routers/seniority";
+import { jobOpeningRouter } from "./routers/job-opening";
 
 export const appRouter = createTRPCRouter({
   applicant: applicantRouter,
   area: areaRouter,
+  role: roleRouter,
+  seniority: seniorityRouter,
+  jobOpening: jobOpeningRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/job-opening/index.ts
@@ -0,0 +1,9 @@
+import {createTRPCRouter} from "~/server/api/trpc";
+
+import { getAllJobOpenings } from "./get-all";
+
+export { getAllJobOpenings };
+
+export const jobOpeningRouter = createTRPCRouter({
+  getAllJobOpenings,
+});
