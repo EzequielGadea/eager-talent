15ebd687e456c8e98d03c5fc5842e45ab4738768
brunovecchio <brunovecchio9@gmail.com>
2026-09-10 16:46:44 -0300

    feature :alta-de-candidato fix : Se crearon los componentes router, core para utilizar uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,24 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const upload = createUploadthing();
+
+export const ourFileRouter = {
+  candidateFiles: upload({
+        /* image: {
+        maxFileSize: "4MB",
+        maxFileCount: 1,
+        }, */
+    pdf: {
+      maxFileSize: "8MB",
+      maxFileCount: 1,
+    },
+  }).onUploadComplete(({ file }) => {
+    return {
+      url: file.url,
+      name: file.name,
+      key: file.key,
+    };
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
