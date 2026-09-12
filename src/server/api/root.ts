41cdd7238de47e7c3ed41be4a20543999c0be2d3
Santiago Cayafa <cayafasantiago@gmail.com>
2026-09-12 01:29:18 -0300

    feat: add candidate profile initial view

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,13 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { candidateRouter } from "./routers/candidate";
+import { applicationRouter } from "./routers/application";
+import { activityRouter } from "./routers/activity";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  candidate: candidateRouter,
+  application: applicationRouter,
+  activity: activityRouter,
+});
 
 export type AppRouter = typeof appRouter;
 
