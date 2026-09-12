bef73363d9da78255c94b3b0934436630b80aa90
Santiago Cayafa <cayafasantiago@gmail.com>
2026-09-12 16:05:08 -0300

    feat: add candidate profile role-based view

--- src/server/api/routers/application/index.ts
+++
@@ -1,6 +0,0 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { getApplicationsByCandidateIdProcedure } from "./get-by-candidate-id";
-
-export const applicationRouter = createTRPCRouter({
-  getByCandidateId: getApplicationsByCandidateIdProcedure,
-});
