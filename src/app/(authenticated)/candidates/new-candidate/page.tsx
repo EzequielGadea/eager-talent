8f262164a9923e0a2d315e6b951cba42c430a0f2
brunovecchio <brunovecchio9@gmail.com>
2026-09-12 20:02:26 -0300

    feature: alta-de-candidato fix: correccion de carpetas y variables candidate a applicant

--- src/app/(authenticated)/candidates/new-candidate/page.tsx
+++
@@ -1,5 +0,0 @@
-import NewCandidateForm from "./_components/new-candidate-form";
-
-export default async function newCandidatePage() {
-  return <NewCandidateForm />;
-}
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const upload = createUploadthing();
 
 export const ourFileRouter = {
-  candidateFiles: upload({
+  applicantFiles: upload({
     image: {
       maxFileSize: "4MB",
       maxFileCount: 1,
