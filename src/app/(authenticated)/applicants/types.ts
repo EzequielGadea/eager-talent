c0b0b424e41b1c029c367b5813e2f2bcb5d6a948
Ezequiel <marcelo.quintana@fing.edu.uy>
2026-09-15 18:22:22 -0300

    feature : paginación

--- src/app/(authenticated)/applicants/types.ts
+++ src/app/(authenticated)/applicants/types.ts
@@ -24,6 +24,8 @@ export interface ApplicantInfo {
   email: string;
 }
 
+export const ITEMS_PER_PAGE = 2;
+
 export type ApplicantsPromise = ReturnType<typeof api.applicant.fetchAll>;
 
 const avatarPalette = [
