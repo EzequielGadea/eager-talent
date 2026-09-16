659e590d8f7a99aef622c9253e6adab4d7666d0a
Santiago <santiagomaguregui1@gmail.com>
2026-09-16 16:56:27 -0300

    feat: busqueda de candidatos con filtrado opcional

--- src/server/api/routers/applicant/fetch-all.ts
+++ src/server/api/routers/applicant/fetch-all.ts
@@ -8,38 +8,79 @@ import { TRPCError } from "@trpc/server";
 
 export const fetchAll = protectedProcedure
     .input(
-        z.object({ //TODO agregar parametros para filtros, todos opcionales
-            //<param1>: z.string().optional()
-            currentPage: z.number(),
-        }).optional()
+        z.object({
+            seniorityId: z.array(z.string()).default([]),
+            areaId: z.array(z.string()).default([]),
+            jobOpeningId: z.array(z.string()).default([]),
+            roleId: z.array(z.string()).default([]),
+            search: z.string().optional(),
+            currentPage: z.number().default(1),
+        })
     )
-    .query(async ({ctx, input}) => {
+    .query(async ({ ctx, input }) => {
         await new Promise((resolve) => setTimeout(resolve, 3000));
-        /*
-        try {
-            //dato de prueba basura
-            const newApplicant = await ctx.db.applicant.create({
-                data: {
-                    id: '1',
-                    name: 'martin',
-                    lastName: "fossatti",
-                    role: {
-                        connect: {
-                            id: "cmu1l7twf0000d9y61l89qj85",
-                        }
-                    }
-                }
-            });
-        } catch (error) { console.log(error); }
-        */
+        const where: Prisma.ApplicantWhereInput = {
+            ...(input?.roleId && input.roleId.length > 0 && {
+                roleId: {
+                    in: input.roleId,
+                },
+            }),
+
+            ...(input?.seniorityId && input.seniorityId.length > 0 && {
+                seniorityId: {
+                    in: input.seniorityId,
+                },
+            }),
+
+            ...(input?.areaId && input.areaId.length > 0 && {
+                areaId: {
+                    in: input.areaId,
+                },
+            }),
+
+            ...(input?.jobOpeningId && input.jobOpeningId.length > 0 && {
+                applications: {
+                    some: {
+                        jobOpeningId: {
+                            in: input.jobOpeningId,
+                        },
+                    },
+                },
+            }),
+
+            ...(input?.search && {
+                OR: [
+                    {
+                        name: {
+                            contains: input.search,
+                            mode: "insensitive",
+                        },
+                    },
+                    {
+                        lastName: {
+                            contains: input.search,
+                            mode: "insensitive",
+                        },
+                    },
+                    {
+                        email: {
+                            contains: input.search,
+                            mode: "insensitive",
+                        },
+                    },
+                ],
+            }),
+        };
+
         const result = await ctx.db.applicant.findMany({
+            where,
             skip: ((input?.currentPage ?? 1) - 1) * 8,
             take: 8,
             orderBy: {
                 id: "asc",
             },
-            include: { 
-                role: { 
+            include: {
+                role: {
                     select: {
                         name: true,
                     }
@@ -58,7 +99,7 @@ export const fetchAll = protectedProcedure
                 },
                 applications: {
                     include: {
-                        jobOpening : {
+                        jobOpening: {
                             select: {
                                 name: true,
                             }
@@ -71,8 +112,8 @@ export const fetchAll = protectedProcedure
                     }
                 },
             },
-        //agregar filtros a la consulta
-    })
+
+        })
         console.log("antes de ir a front");
-        return {applicants: result};
+        return { applicants: result };
     })
