bd3cd49f53f9cbcd0d69ce78cfb4d3059fa5ca6e
Santiago <santiagomaguregui1@gmail.com>
2026-09-17 19:36:11 -0300

    feature: arreglos de prettier, se agrega logica con router, searchParams y Pathname

--- src/app/(authenticated)/applicants/_components/applicant-table.tsx
+++ src/app/(authenticated)/applicants/_components/applicant-table.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import {
   Table,
@@ -12,60 +13,49 @@ import {
 
 import { ApplicantPagination } from "./applicant-pagination";
 import { ApplicantInfo } from "../types";
-import { transformApplicants } from "../utils";
 import { ApplicantRow } from "./applicant-row";
-import { ITEMS_PER_PAGE } from "../constants";
-import { getApplicantsPage } from "../utils";
 import { TableFallback } from "./fallbacks";
 
-export function ApplicantTable(props : {applicantsData : ApplicantInfo[], countApplicants : number}) {
-  const [applicantsData, setApplicants] = useState(props.applicantsData)
-    const [currentPage, setCurrentPage] = useState(1);
-    const paginatedApplicants = props.applicantsData?.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
+export function ApplicantTable(props: {
+  applicantsData: ApplicantInfo[];
+  countApplicants: number;
+  currentPage: number;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function updateRow(page: number) {
-    setIsLoading(true);
-
-    try {
-      const raw = getApplicantsPage(page);
-      const { applicantsData: newApplicants } = await transformApplicants(raw);
-
-      setCurrentPage(page);
-      setApplicants(newApplicants);
-    } finally {
-      setIsLoading(false);
-    }
+  function updatePage(page: number) {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", String(page));
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
   }
 
-    return (
-      <div className="flex min-h-screen flex-col">  
-        <div className="w-full overflow-x-auto rounded-xl border border-dashboard-border bg-white shadow-sm">
-          <Table className="min-w-262.5 table-fixed">
-          <ApplicantTableHeader/>
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="w-full overflow-x-auto rounded-xl border border-dashboard-border bg-white shadow-sm">
+        <Table className="min-w-262.5 table-fixed">
+          <ApplicantTableHeader />
 
-            <TableBody className="divide-y divide-dashboard-border">
-              { isLoading
-              ? <TableFallback/>
-              :applicantsData?.map((applicant) => (
-                <ApplicantRow key={applicant.id} applicant={applicant} />
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="mt-auto">
-          <ApplicantPagination
-            countApplicants={props.countApplicants}
-            currentPage={currentPage}
-            onPageChange={updateRow}
-          />
-        </div>
+          <TableBody className="divide-y divide-dashboard-border">
+            {props.applicantsData.length === 0 ? <TableFallback /> : props.applicantsData.map((applicant) => (
+              <ApplicantRow key={applicant.id} applicant={applicant} />
+            ))}
+          </TableBody>
+        </Table>
       </div>
-    )
+      <div className="mt-auto">
+        <ApplicantPagination
+          countApplicants={props.countApplicants}
+          currentPage={props.currentPage}
+          onPageChange={updatePage}
+        />
+      </div>
+    </div>
+  )
 }
 
-export function ApplicantTableHeader(){
+export function ApplicantTableHeader() {
   return (
     <TableHeader>
       <TableRow className="border-b border-dashboard-border hover:bg-transparent">
