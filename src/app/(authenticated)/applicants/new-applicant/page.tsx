97348520e30e694d72460058c62795b2e3aeceaa
brunovecchio <brunovecchio9@gmail.com>
2026-09-19 01:50:04 -0300

    feature: alta-de-candidato fix: rebase con dev e implementacion de permisos en new applicant

--- src/app/(authenticated)/applicants/new-applicant/page.tsx
+++ src/app/(authenticated)/applicants/new-applicant/page.tsx
@@ -1,7 +1,35 @@
 import { Suspense } from "react";
 import NewApplicantForm from "./_components/new-applicant-form";
+import { TRPCError } from "@trpc/server";
+import { auth } from "~/lib/auth";
+import { headers } from "next/headers";
 
-export default async function newApplicantPage() {
+import { redirect } from "next/navigation";
+
+export default function newApplicantPage() {
+  return(
+    <Suspense>
+      <ProtectedNewApplicantPage/>
+    </Suspense>
+  )
+}
+
+
+
+
+
+async function ProtectedNewApplicantPage(){ 
+    const permission = await auth.api.hasPermission({
+    headers: await headers(),
+    body: {
+      permissions:{
+        applicant:["create"],
+        },
+      },
+    });
+  if (!permission.success) {
+    redirect("/dashboard");
+  }
   return (
     <>
       <div className="mx-auto flex w-full max-w-4xl flex-col gap-4 p-4 mb-0">
